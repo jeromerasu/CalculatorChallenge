1295fa3f47b515ca7869e2a963dc2b418f192d07
jeromerasu <42827814+jeromerasu@users.noreply.github.com>
2019-09-13 17:38:54 -0700

    Add project files.

---
+++ CalculatorChallenge/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CalculatorChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
